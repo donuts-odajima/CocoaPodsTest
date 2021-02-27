5656f3caa3cbc046a7ba29d54c6bc4ec78b2af12
Naoki Odajima <odajima.naoki@donuts.ne.jp>
2021-02-27 21:01:42 +0900

    Initial Commit

---
+++ CocoaPodsTest/CocoaPodsTest.h
@@ -0,0 +1,18 @@
+//
+//  CocoaPodsTest.h
+//  CocoaPodsTest
+//
+//  Created by mixch-dev on 2021/02/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaPodsTest.
+FOUNDATION_EXPORT double CocoaPodsTestVersionNumber;
+
+//! Project version string for CocoaPodsTest.
+FOUNDATION_EXPORT const unsigned char CocoaPodsTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPodsTest/PublicHeader.h>
+
+
